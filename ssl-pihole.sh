bb32d98c68666972fa35cc85d969982f4e29e8e4
Iqbal <iqbalfaf127@gmail.com>
2020-02-10 09:57:48 +0700

    Cloudflare Pihole Installer
    Cloudflared dns

--- ssl-pihole.sh
+++ ssl-pihole.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-echo"================================================"
-echo"============= DNS Over SSL PIHOLE =============="
-echo"============== Installer Armbian ==============="
-echo"================================================"
-echo""
+echo "================================================"
+echo "============= DNS Over SSL PIHOLE =============="
+echo "============== Installer Armbian ==============="
+echo "================================================"
+echo ""
 echo "Konfigurasi Arsitektur "
 sudo dpkg --add-architecture armhf >/dev/null 2>&1
 echo "Konfigurasi Arsitektur Selesai"
 echo "Update Repo"
 sudo apt-get update >/dev/null 2>&1
 echo "Update Repo Selelsai"
-echo"Install libc6"
+echo "Install libc6"
 sudo apt-get install libc6:armhf >/dev/null 2>&1
 echo "Install libc6 Selesai"
-echo"Mengunduh File SSL"
+echo "Mengunduh File SSL"
 wget https://bin.equinox.io/c/VdrWdbjqyF/cloudflared-stable-linux-arm.tgz >/dev/null 2>&1
 tar -xvzf cloudflared-stable-linux-arm.tgz >/dev/null 2>&1
 sudo cp ./cloudflared /usr/local/bin >/dev/null 2>&1
 sudo chmod +x /usr/local/bin/cloudflared >/dev/null 2>&1
-echo"Mengunduh File SSL Selesai"
+echo "Mengunduh File SSL Selesai"
 echo "Cek Versi Cloudflared"
 cloudflared -v
 echo "Membuat User Cloudflared"
